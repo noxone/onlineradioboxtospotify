eb11c6b10bd5c9aa9ea9c216c9dc55a91b8458b8
Olaf Neumann <github@olafneumann.org>
2023-11-21 21:36:29 +0100

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -5,10 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "OnlineRadioBoxToSpotify",
+    products: [
+        .executable(name: "OnlineRadioBoxToSpotify", targets: ["OnlineRadioBoxToSpotify"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from: "0.3.0"))
+    ],
     targets: [
+        .target(
+            name: "OnlineRadioBoxToSpotify",
+            dependencies: [
+                .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+            ]
+        ),
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "OnlineRadioBoxToSpotify"),
+        //.executableTarget(name: "OnlineRadioBoxToSpotify"),
     ]
 )
