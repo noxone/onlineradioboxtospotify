122385c71bea3df5454696bd0f7e913419f3cc71
Olaf Neumann <github@olafneumann.org>
2023-11-29 23:15:34 +0100

    Remove unused dependdency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from: "0.3.0")),
-        .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.19.0")),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", .upToNextMajor(from: "2.6.1")),
         .package(url: "https://github.com/Peter-Schorn/SpotifyAPI.git", .upToNextMajor(from: "2.2.4")),
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", .upToNextMajor(from: "0.2.0")),
@@ -23,7 +22,6 @@ let package = Package(
             name: "OnlineRadioBoxToSpotify",
             dependencies: [
                 .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
-                .product(name: "AsyncHTTPClient", package: "async-http-client"),
                 .product(name: "SwiftSoup", package: "SwiftSoup"),
                 .product(name: "CollectionConcurrencyKit", package: "CollectionConcurrencyKit"),
                 .product(name: "SpotifyAPI", package: "SpotifyAPI"),
