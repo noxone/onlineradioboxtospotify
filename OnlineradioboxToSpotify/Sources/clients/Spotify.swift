8d83817b9096f2d5bd754409e021aa3c7f7a9867
Olaf Neumann <github@olafneumann.org>
2023-12-17 22:01:41 +0100

    Enhance log output

--- OnlineradioboxToSpotify/Sources/clients/Spotify.swift
+++ OnlineradioboxToSpotify/Sources/clients/Spotify.swift
@@ -84,21 +84,29 @@ class Spotify {
         let existingItems = try await getPlaylistTrackUris(for: playlistUri)
         
         let patches = extendedPatch(from: existingItems, to: tracksToSet)
+        
         var list = Array(existingItems)
-        logger.info("Working on \(patches.count) patches...")
+        var deletions = 0
+        var insertions = 0
+        var moves = 0
+        logger.info("Working on \(patches.count) patch(es)...")
         for patch in patches {
             switch patch {
             case .deletion(index: let index):
                 let item = list.remove(at: index)
                 _ = try await spotify.removeSpecificOccurrencesFromPlaylist(playlistUri, of: URIsWithPositionsContainer(urisWithPositions: [URIWithPositions(uri: item, positions: [index])])).async()
+                deletions += 1
             case .insertion(index: let index, element: let element):
                 list.insert(element, at: index)
                 _ = try await spotify.addToPlaylist(playlistUri, uris: [element], position: index).async()
+                insertions += 1
             case .move(from: let oldIndex, to: let newIndex):
                 list.move(fromOffsets: IndexSet([oldIndex]), toOffset: newIndex + (newIndex > oldIndex ? 1 : 0))
                 _ = try await spotify.reorderPlaylistItems(playlistUri, body: ReorderPlaylistItems(rangeStart: oldIndex, insertBefore: newIndex + (newIndex > oldIndex ? 1 : 0))).async()
+                moves += 1
             }
         }
+        logger.info("Handled \(insertions) insertion(s), \(deletions) deletion(s) and \(moves) move(s).")
     }
 
     private func getPlaylist(withName name: String) async throws -> Playlist<PlaylistItemsReference>? {
